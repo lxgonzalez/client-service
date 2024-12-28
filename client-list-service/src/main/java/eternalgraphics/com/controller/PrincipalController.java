a394b12225e498334527b29b75130804df50dfb2
programmingroup <luisgx14@gmail.com>
2024-12-27 21:55:34 -0500

    feat: :sparkles: Created verificator for microservice

---
+++ client-list-service/src/main/java/eternalgraphics/com/controller/PrincipalController.java
@@ -0,0 +1,25 @@
+package eternalgraphics.com.controller;
+
+import eternalgraphics.com.model.Client;
+import eternalgraphics.com.service.IClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class ClientController {
+    @Autowired
+    private IClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<String> checkService(){
+        return new ResponseEntity<>("List Client microservice is running...", HttpStatus.OK);
+    }
+
+}
