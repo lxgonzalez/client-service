81963fc9dd7d72099b281bc77b139944fba0b1a6
programmingroup <luisgx14@gmail.com>
2024-12-27 21:56:03 -0500

    feat: :sparkles: Created getters and setters

--- client-list-service/src/main/java/eternalgraphics/com/model/Client.java
+++ client-list-service/src/main/java/eternalgraphics/com/model/Client.java
@@ -27,4 +27,43 @@ public class Client {
     @Column(length = 250, nullable = false)
     private String address;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
